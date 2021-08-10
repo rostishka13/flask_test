c84a81a810ff4212767a66f31c356c2cd435c603
Rostyk <rostishka1@gmail.com>
2021-08-10 12:36:53 +0300

    addin login and register templates

--- app.py
+++ app.py
@@ -1,19 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, current_user
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+
+# config
 app = Flask(__name__)
 login = LoginManager(app)
-
-
-app.config["FLASK_ADMIN_SWATHC"] = "cerulean"
-
-
 db = SQLAlchemy(app)
+app.config["FLASK_ADMIN_SWATHC"] = "cerulean"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "thisismykey"
+
+
+# authentication
+class RegisterForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "username"},
+    )
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "password"},
+    )
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "That usernmae already exist. Please choose a different one"
+            )
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "username"},
+    )
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "password"},
+    )
+    submit = SubmitField("Login")
 
 
 @login.user_loader
@@ -21,14 +55,29 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
+# route
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
+
+
+# model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 
 class Category(db.Model):
@@ -51,9 +100,13 @@ class Product(db.Model):
         return f"product {self.title}"
 
 
+# admin settings
 class MyAdminIndexView(AdminIndexView):
     def is_accessible(self):
         return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("index"))
 
 
 admin = Admin(
